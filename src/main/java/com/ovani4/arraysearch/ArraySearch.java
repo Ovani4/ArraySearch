e90efec69515361b4a1c3eb156e195f454bb9f26
Ovani4 <ovani4@ya.ru>
2021-02-08 01:25:59 +0300

    Create project and method IsUnic

---
+++ src/main/java/com/ovani4/arraysearch/ArraySearch.java
@@ -0,0 +1,26 @@
+package main.java.com.ovani4.arraysearch;
+
+public class ArraySearch {
+    private static boolean IsUnic (int[] array){
+        int iter = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]){
+                    iter++;
+                }
+            }
+        }
+        System.out.println(iter);
+        if (iter > 0){
+            return false;
+        } else return true;
+    }
+    public static void main(String[] args) {
+        ArraySearch as = new ArraySearch();
+        int [] array = {15,2,5,8,9,7,17};
+        int [] array1 = {15,2,7,15,9,7,17};
+        System.out.println(as.IsUnic(array));
+        System.out.println(as.IsUnic(array1));
+        System.out.println(as.IsUnic(array));
+    }
+}

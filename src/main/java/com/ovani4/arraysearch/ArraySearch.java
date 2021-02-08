482c1d8f77fb82069d06ebaaa9c2152549151d38
Ovani4 <ovani4@ya.ru>
2021-02-08 14:03:30 +0300

    change method SearchSum - work.

--- src/main/java/com/ovani4/arraysearch/ArraySearch.java
+++ src/main/java/com/ovani4/arraysearch/ArraySearch.java
@@ -1,5 +1,7 @@
 package main.java.com.ovani4.arraysearch;
 
+import java.util.*;
+
 public class ArraySearch {
     private static boolean IsUnic (int[] array){
         int iter = 0;
@@ -15,10 +17,37 @@ public class ArraySearch {
             return false;
         } else return true;
     }
+
+    private void SearchSum(int[] array){
+        Set<Integer> set = new TreeSet<>();
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i : array) {
+            list.add(i);
+        }
+        //Добавление массива в Set
+        for (int i : array) {
+            if (!set.add(i)) {
+                System.out.println("Значение " + i + " не уникально"); //Вместо метода IsUnic
+            } else set.add(i);
+        }
+        int x = list.size();
+        int y = array[x-1];
+        //Поиск значений
+        for (int i : set) {
+            for (int j : set) {
+                if (y == (i + j)){
+                    if (list.indexOf(i) < list.indexOf(j))
+                    System.out.println(list.indexOf(i) + " "
+                            + list.indexOf(j));
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         ArraySearch as = new ArraySearch();
         int [] array = {15,2,5,8,9,7,17};
         int [] array1 = {15,2,7,15,9,7,17};
-
+        as.SearchSum(array);
     }
 }

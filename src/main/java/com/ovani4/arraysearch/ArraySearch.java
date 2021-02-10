058ed9c2b195a8aa27e29c281bd22313eca23321
Ovani4 <ovani4@ya.ru>
2021-02-10 20:05:20 +0300

    its ok

--- src/main/java/com/ovani4/arraysearch/ArraySearch.java
+++ src/main/java/com/ovani4/arraysearch/ArraySearch.java
@@ -4,25 +4,19 @@ import java.util.*;
 
 public class ArraySearch {
 
-    private void SearchSum(int[] array)  {
-    Set<Integer> set = new TreeSet<>();
-    boolean bo = false;
+    private boolean SearchDuplicates(int[] array) {
+        Set<Integer> set = new TreeSet<>();
         for (int i : array) {
-            if (!set.add(i)){
-                bo = true;
-                System.out.println(bo);
-                break;
+            if (!set.add(i)) {
+                return true;
             }
-        }if (!bo)
-        System.out.println(bo);
+        }return false;
     }
-
-    private void SearchDouble(int[] array) {
+    private void SearchSum(int[] array, int x) {
         Map<Integer,Integer> map = new TreeMap<>();
              for (int i = 0; i<array.length;i++) {
                  map.put(i, array[i]);
              }
-             int x = array[array.length-1];
         for (Map.Entry entry : map.entrySet()) {
             for (Map.Entry entry1 : map.entrySet()) {
                 if (entry1.getValue().equals(x - (int) entry.getValue())
@@ -41,8 +35,8 @@ public class ArraySearch {
         int [] array1 = {15,2,7,15,9,7,17};
 
 
-        as.SearchSum(array1);
-        as.SearchDouble(array);
+        as.SearchSum(array1, array[array.length-1]);
+        as.SearchDuplicates(array);
 
     }
 }
